ed0b1b9d4fcfe86ff42aa52b0c43d3a954ef0c0b
shuvam <shubhamduttadnk@gmail.com>
2022-10-09 07:34:25 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = 'Shuvam'
+const lastname = 'Dutta'
+
+const date = new Date().toLocaleDateString();
+const time = new Date().toLocaleTimeString();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+
+  <React.Fragment>
+    <h1>{`${name} ${lastname}`}</h1>
+    <p>{date}</p>
+    <h4>{time}</h4>
+  </React.Fragment>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
